b2b51f66d28bf18cf6c2f131b95eb9b33bf6f597
Adonis-Dulaurent <adonis.dulaurent@gmail.com>
2025-03-19 15:48:23 +0100

    résolution de l'erreur de l'acceuil

--- app/models/formulaires.py
+++ app/models/formulaires.py
@@ -2,5 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, TextAreaField, PasswordField
 class AjoutUtilisateur(FlaskForm):
     #Formulaire pour ajouter un utilisateur 
-    identifiant=StringField("identifiant", validators=[])
+    identifiant=StringField("prenom", validators=[])
     password=PasswordField("password", validators=[])
+
+class Connexion(FlaskForm):
+    prenom = StringField("prenom", validators=[])
+    password = PasswordField("password", validators=[])
---
+++ app/routes/users.py
@@ -0,0 +1,24 @@
+from flask import url_for, render_template, redirect, request, flash
+from ..models import users
+from ..models.formulaires import AjoutUtilisateur
+from ..utils.transformations import clean_arg
+from ..app import app, db
+
+@app.route("/utilisateurs/ajout", methods=["GET", "POST"])
+def ajout_utilisateur():
+    form =AjoutUtilisateur()
+
+    if form.validate_on_submit():
+        statut, donnees = Users.ajout(
+            prenom=clean_arg(request.form.get("prenom", None)),
+            password=clean_arg(request.form.get("password", None))
+        )
+
+        if statut is True: 
+            flash("Ajout effectué", "success")
+            return redirect(url_for("acceuil"))
+        else:
+            flash(",".join(donnees), "error")
+            return render_template("pages/ajout_utilisateur.html", form=form)
+    else:
+        return render_template("pages/ajout_utilisateur.html", form=form)
