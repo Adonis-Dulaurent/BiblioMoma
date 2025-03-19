a13ae2d52114b28720b3941614aa071e461f1d6c
Adonis-Dulaurent <adonis.dulaurent@gmail.com>
2025-03-19 18:08:20 +0100

    Ajout user work

--- app/models/formulaires.py
+++ app/models/formulaires.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, TextAreaField, PasswordField
+
 class AjoutUtilisateur(FlaskForm):
     #Formulaire pour ajouter un utilisateur 
-    identifiant=StringField("prenom", validators=[])
+    pseudo=StringField("pseudo", validators=[])
     password=PasswordField("password", validators=[])
+    email=StringField("email", validators=[])
 
 class Connexion(FlaskForm):
     prenom = StringField("prenom", validators=[])
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,17 +1,20 @@
 from flask import url_for, render_template, redirect, request, flash
-from ..models import users
+from flask_login import login_user, current_user
+from ..models.users import User
 from ..models.formulaires import AjoutUtilisateur
 from ..utils.transformations import clean_arg
-from ..app import app, db
+from ..app import app, db, login
 
 @app.route("/utilisateurs/ajout", methods=["GET", "POST"])
 def ajout_utilisateur():
+
     form =AjoutUtilisateur()
 
     if form.validate_on_submit():
-        statut, donnees = Users.ajout(
-            prenom=clean_arg(request.form.get("prenom", None)),
-            password=clean_arg(request.form.get("password", None))
+        statut, donnees = User.ajout(
+            pseudo=clean_arg(request.form.get("pseudo", None)),
+            password=clean_arg(request.form.get("password", None)),
+            email=clean_arg(request.form.get("email", None))
         )
 
         if statut is True: 
@@ -22,3 +25,29 @@ def ajout_utilisateur():
             return render_template("pages/ajout_utilisateur.html", form=form)
     else:
         return render_template("pages/ajout_utilisateur.html", form=form)
+
+@app.route("/utilisateurs/connexion", methods=["GET","POST"])
+def connexion():
+    form = connexion()
+
+    if current_user.is_authenticated is True:
+        flash("Vous êtes déjà connecté", "info")
+        return redirect(url_for("accueil"))
+
+    if form.validate_on_submit():
+        utilisateur = User.identification(
+            prenom=clean_arg(request.form.get("prenom", None)),
+            password=clean_arg(request.form.get("password", None))
+        )
+        if utilisateur:
+            flash("Connexion effectuée", "success")
+            login_user(utilisateur)
+            return redirect(url_for("accueil"))
+        else:
+            flash("Les identifiants n'ont pas été reconnus", "error")
+            return render_template("pages/connexion.html", form=form)
+
+    else:
+        return render_template("pages/connexion.html", form=form)
+
+login.login_view = 'connexion'
