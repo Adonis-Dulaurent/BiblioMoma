197926b8f948998cd59921d4a9d78f1d57c48a50
Camille <camille.batard@chartes.psl.eu>
2025-03-19 15:44:52 +0100

    Fiches artistes-oeuvres

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -9,6 +9,11 @@ from ..models.mapping import *
 def accueil():
     return render_template("pages/index.html")
 
+@app.route('/cent_oeuvres_artistes')
+def cent_oeuvres_artistes():
+    return render_template("pages/100_oeuvres_artistes.html")
+
+
 @app.route('/test_db')
 def test_db():
     """
