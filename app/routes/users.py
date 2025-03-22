c965fd2981e5507d1e6c643734309866c6f33876
Adonis-Dulaurent <adonis.dulaurent@gmail.com>
2025-03-22 16:01:52 +0100

    ajout du panier

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,6 +1,6 @@
 from flask import url_for, render_template, redirect, request, flash
 from flask_login import login_user, current_user, logout_user
-from ..models.users import User
+from ..models.users import User, Panier
 from ..models.formulaires import AjoutUtilisateur, Connexion
 from ..utils.transformations import clean_arg
 from ..app import app, db, login
@@ -19,7 +19,7 @@ def ajout_utilisateur():
 
         if statut is True: 
             flash("Ajout effectué", "success")
-            return redirect(url_for("acceuil"))
+            return redirect(url_for("index"))
         else:
             flash(",".join(donnees), "error")
             return render_template("pages/ajout_utilisateur.html", form=form)
@@ -32,7 +32,7 @@ def connexion():
 
     if current_user.is_authenticated is True:
         flash("Vous êtes déjà connecté", "info")
-        return redirect(url_for("accueil"))
+        return redirect(url_for("index"))
 
     if form.validate_on_submit():
         utilisateur = User.identification(
@@ -43,7 +43,7 @@ def connexion():
         if utilisateur:
             flash("Connexion effectuée", "success")
             login_user(utilisateur)
-            return render_template(url_for("accueil"))
+            return redirect(url_for("guide"))
         else:
             flash("Les identifiants n'ont pas été reconnus", "error")
             return render_template("pages/connexion.html", form=form)
@@ -56,7 +56,19 @@ def deconnexion():
     if current_user.is_authenticated is True:
         logout_user()
     flash("vous êtes déconnecté", "info")
-    return redirect(url_for("acceuil"))
+    return redirect(url_for("guide"))
 
 login.login_view = 'connexion'
 
+@app.route("/utilisateurs/panier")
+def afficher_panier():  
+    """
+    afficher le panier de l'utilisateur 
+    """
+    #verification si l'utilisateur est connecté 
+    if not current_user.is_authenticated:
+        flash("You must be logged in to access your basket")
+        return redirect(url_for("connexion", next=request.path))
+        
+    panier = Panier.obtenir_panier_utilisateur(current_user.id)
+    return render_template('pages/panier.html', panier=panier)
